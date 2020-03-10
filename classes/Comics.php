7a0afc1842c0e0033a7052f8798ac0cbee3d293b
Matteo Magliano <matteo.magliano01@gmail.com>
2020-03-10 17:16:31 +0100

    exte

--- classes/Comics.php
+++ classes/Comics.php
@@ -1,6 +1,14 @@
 <?php 
   require_once 'Graphic.php';
 
+  class Comics extends Graphic 
+  {
+    public function __construct($_title, $_genre = 'Comics', $_page, $_price, $_publishing)
+    {
+      parent::__construct($_title, $_genre, $_page, $_price, $_publishing);
+    }
+  }
+
   
 
 
--- index.php
+++ index.php
@@ -4,6 +4,6 @@ require_once 'classes/Graphic.php';
 require_once 'classes/Comics.php';
 require_once 'classes/Manga.php';
 
-$thor = new Graphic('Thor', 'Comics', '250', '5.00€', 'Marvel');
+$thor = new Comics('Thor', 'Comics', 250, '5.00€', 'Marvel');
 
 var_dump($thor);
